55f7be1d2eaf10d3cfb32924eede32d3727bb612
vajrabilash <degapoguvajrabilash@gmail.com>
2024-05-28 09:02:38 +0530

    java basic helloworld

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+// java basic program to print helloworld
+class helloworld{  // main class declaration
+	public static void main(String ar[]){     // public class
+			System.out.println("Helloworld");   // printing helloworld
+	}
+}
+//end of the program
